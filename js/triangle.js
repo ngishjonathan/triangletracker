4ad78b74cc182538234aacbdefaef0db9e5f4a77
ngishjonathan <balozimwash@gmail.com>
2019-03-22 16:29:39 +0300

    added headers

---
+++ js/triangle.js
@@ -0,0 +1,6 @@
+length 1 =parseInt(document.getElementById('length 1').value;
+length 2 =parseInt(document.getElementById('length 2').value;
+length 3 =parseInt(document.getElementById('length 3').value;
+if (length 1 === length 2 && length 1 === length 3) {
+  answer.textcontent =("All sides are equal:Equlateral");
+}
