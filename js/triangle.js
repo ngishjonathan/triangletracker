befd5dd40495bd2610aed7352af1b27bbac4f770
ngishjonathan <balozimwash@gmail.com>
2019-03-25 07:05:36 +0300

    corrected the js function

--- js/triangle.js
+++ js/triangle.js
@@ -1,26 +1,27 @@
 function findOut() {
-  var sideC =prompt("Enter sideA")
+  var sideA =prompt("Enter sideA")
   var sideB =prompt("Enter sideB")
   var sideC =prompt("Enter sideC")
-}
+
 if (sideA === sideB && sideA === sideC) {
-alert("Equilateral")
+alert("Equilateral");
 }
 else if (sideA === sideB && sideB !== sideC) {
-alert("isorceless")
+alert("isorceless");
 }
 else if (sideB === sideC && sideB !== sideA) {
-alert("isorceless")
+alert("isorceless");
 }
 else if (sideC === sideB && sideC !== sideA) {
-alert("isorceless")
+alert("isorceless");
 }
 else if (sideA !== sideB && sideA !== sideC) {
-alert("scalene")
+alert("scalene");
 }
 else if (sideB !== sidec && sideC !== sideA) {
-alert("scalene")
+alert("scalene");
 }
 else {
-alert("not a triangle")
+alert("not a triangle");
 }
+};
