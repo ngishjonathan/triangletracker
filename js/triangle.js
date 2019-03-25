51eb4b91a7cb8531ec285adc4ad76ed1dbfc4a87
ngishjonathan <balozimwash@gmail.com>
2019-03-25 08:08:29 +0300

    final commit

---
+++ js/triangle.js
@@ -0,0 +1,27 @@
+function findOut() {
+  var sideA =prompt("Enter sideA")
+  var sideB =prompt("Enter sideB")
+  var sideC =prompt("Enter sideC")
+
+if (sideA === sideB && sideA === sideC) {
+alert("Equilateral");
+}
+else if (sideA === sideB && sideB !== sideC) {
+alert("isorceless");
+}
+else if (sideB === sideC && sideB !== sideA) {
+alert("isorceless");
+}
+else if (sideC === sideB && sideC !== sideA) {
+alert("isorceless");
+}
+else if (sideA !== sideB && sideA !== sideC) {
+alert("scalene");
+}
+else if (sideB !== sidec && sideC !== sideA) {
+alert("scalene");
+}
+else {
+alert("not a triangle");
+}
+};
